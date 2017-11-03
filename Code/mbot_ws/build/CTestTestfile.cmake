d1b2cb77b752a77bf3690f5afc1a08cf7339fa78
Ler Wilson <lws803@gmail.com>
2017-11-03 12:09:35 +0800

    Removed ROS ws <no need for gazebo simulations>

--- Code/mbot_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wilson/mbot_ws/src
-# Build directory: /home/wilson/mbot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(mbot_simulation_environments)
-subdirs(beginner_tutorials)
