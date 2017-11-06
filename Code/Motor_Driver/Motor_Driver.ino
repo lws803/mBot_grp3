7a45989bda64a08f0517bdc37225949cee64ab98
Ler Wilson <lws803@gmail.com>
2017-11-06 23:54:29 +0800

    Added some control structure for mBot

--- Code/Motor_Driver/Motor_Driver.ino
+++ Code/Motor_Driver/Motor_Driver.ino
@@ -1,5 +1,8 @@
 #include "MeOrion.h"
 #include <PID_v1.h>
+#define TIMEOUT 30000
+#define ULTRASONIC_SENSOR_PIN 12
+
 
 MeDCMotor motorL(M1);
 MeDCMotor motorR(M2);
@@ -12,7 +15,7 @@ double Output, Output2, Input, Input2, Setpoint, Setpoint2;
 PID myPID(&Input, &Output, &Setpoint,1,0,0, DIRECT);
 PID myPID2(&Input2, &Output2, &Setpoint2, 3, 0, 0, DIRECT);
 
-
+/**
 //stand by for some time
 void statically(int duration) {
   motorL.stop();
@@ -31,6 +34,8 @@ void backward(int duration) {
   motorR.run(-motorSpeedR);
   delay(duration);
 }
+// DEPRECATED
+*/
 //time required for turning: ms
 void left() {
   motorL.run(motorSpeedL);
@@ -42,6 +47,55 @@ void right() {
   motorL.run(-motorSpeedL);
   motorR.run(-motorSpeedR);
   delay(717);
+}
+
+void pid_move_straight (uint8_t desired_spd) {
+  // PID controls
+  Input = analogRead(0); // Left - value gets higher when it gets closer 
+  Input2 = analogRead(3); // Right - value gets higher when it gets closer 
+ 
+  myPID.Compute();
+  myPID2.Compute();
+
+ /**
+  Serial.print("Left: ");
+  Serial.print(" ");
+  Serial.print (Input);
+  Serial.print(" ");
+  Serial.print (Output);
+  Serial.print(" ");
+  Serial.print("Right: ");
+  Serial.print ("");
+  Serial.print (Input2);
+  Serial.print(" ");
+  Serial.println (Output2);
+  */
+
+  
+  // Motor controls  
+  motorL.run(-Output/3 - desired_spd);
+  motorR.run(Output2 + desired_spd);
+}
+
+int encouter(void) {
+  /**
+   * 0 means no encounters 
+   * 1 means encounter wall with ultrasonic sensor  
+   * 2 means encouter black line 
+   */
+   pinMode(ULTRASONIC_SENSOR_PIN, OUTPUT);
+   digitalWrite(ULTRASONIC_SENSOR_PIN, LOW);
+   delayMicroseconds(2);
+   digitalWrite(ULTRASONIC_SENSOR_PIN, HIGH);
+   delayMicroseconds(2);
+   digitalWrite(ULTRASONIC_SENSOR_PIN, LOW);
+   pinMode(ULTRASONIC_SENSOR_PIN, INPUT);
+   duration = pulseIn(ULTRASONIC_SENSOR_PIN, HIGH, TIMEOUT);
+   distance = duration / 2.0 * 0.034;
+   if(distance < 10.0) {
+     return 1; // Encounter wall
+   }
+  return 0; // No encounters by default 
 }
 
 void setup()
@@ -58,28 +112,25 @@ void setup()
 
 void loop()
 {
-  // PID controls
-  Input = analogRead(0); // Left - value gets higher when it gets closer 
-  Input2 = analogRead(3); // Right - value gets higher when it gets closer 
- 
-  myPID.Compute();
-  myPID2.Compute();
+  int state = encounter();
+  while (!state) {
+    pid_move_straight(100);
+  }else {
+    // Stop motor
+    motorL.run(0);
+    motorR.run(0);
+  }
 
- 
-  Serial.print("Left: ");
-  Serial.print(" ");
-  Serial.print (Input);
-  Serial.print(" ");
-  Serial.print (Output);
-  Serial.print(" ");
-  Serial.print("Right: ");
-  Serial.print ("");
-  Serial.print (Input2);
-  Serial.print(" ");
-  Serial.println (Output2);
-  
-  // Motor controls  
-  motorL.run(-Output/3 - 100);
-  motorR.run(Output2 + 100);
+  // Check state and determine action if none, continue with loop
+  switch (state) {
+    case 1: 
+    break;
+    case 2: 
+    break;
+  }
 }
+
+
+
+
 
