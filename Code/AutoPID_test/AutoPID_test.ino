7427e7deed1073b142fb6263bd674d9727d5dd0e
Ler Wilson <lws803@gmail.com>
2017-11-08 16:53:28 +0800

    Made some adjustments to PIDs

--- Code/AutoPID_test/AutoPID_test.ino
+++ Code/AutoPID_test/AutoPID_test.ino
@@ -1,39 +1,43 @@
-#include "AutoPID.h"
 #include "MeOrion.h"
-
+#include <PID_v1.h>
 #define IR_SIDE_L A1
 #define IR_SIDE_R A0
-#define setpointL  600
-#define setpointR 450
-#define OUTPUT_MIN 50
-#define OUTPUT_MAX 50
+
+#define runMotor 1
 
 // Motor setups
 MeDCMotor motorL(M1);
 MeDCMotor motorR(M2);
 
-double outputL, outputR, inputL, inputR, 
-AutoPID myPID_left(&inputL, &setPoint, &outputL, OUTPUT_MIN, OUTPUT_MAX, 1, 1, 2);
-AutoPID myPID_right(&inputR, &setPoint, &outputR, OUTPUT_MIN, OUTPUT_MAX, 1, 1, 2);
+double outputL, outputR, inputL, inputR, setpointL, setpointR; 
 
+PID myPID_L(&inputL, &outputL, &setpointL,1,1,2, DIRECT);
+PID myPID_R(&inputR, &outputR, &setpointR,1,1,2, DIRECT);
 
 void setup() {
+  setpointL = 630;
+  setpointR = 630;
+  myPID_L.SetMode(AUTOMATIC);
+  myPID_R.SetMode(AUTOMATIC);
   Serial.begin (9600);
 }
 
 void loop() {
     inputL = analogRead(IR_SIDE_L);
     inputR = analogRead (IR_SIDE_R);
-    myPID_left.run();
-    myPID_right.run();
 
-    Serial.print (inputL);
-    Serial.print (" ");
-    Serial.print (inputR);
-    
+    myPID_L.Compute();
+    myPID_R.Compute();
     // Motor controls
-    motorL.run(-(outputL + 140));
-    motorR.run(outputR + 140);
-    
-  
+    switch (runMotor) {
+      case 1: 
+      motorL.run(-(outputL + 140));
+      motorR.run(outputR + 140);
+      break;
+      case 0:
+      Serial.print (outputL);
+      Serial.print (" ");
+      Serial.println(outputR);
+      break;
+    }
 }
